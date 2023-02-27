3117b954f309bd8fab487bb9f65c55d00d0b9e7f
Matthew Bronstein <mbronstein1@gmail.com>
2023-02-27 17:45:21 -0600

    initial logic for storing data in firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,6 +6,23 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const showCart = useSelector(state => state.showCart);
+  const cart = useSelector(state => {
+    return {
+      numberOfItemsInCart: state.numberOfItemsInCart,
+      itemsInCart: state.itemsInCart,
+      totalPrice: state.totalPrice,
+    };
+  });
+
+  useEffect(() => {
+    fetch('https://react-http-practice-54ed4-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
